ddaa632ed549cf0507e05ef2c57e4f8a85092465
Gergo Fazekas <144519526+gergofazekas92@users.noreply.github.com>
2024-07-22 14:58:26 +0200

    add correct version to drivers

--- src/main/java/utils/Util.java
+++ src/main/java/utils/Util.java
@@ -7,38 +7,38 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 
 public class Util {
     private static final String GRID_URL = "http://localhost:4444/";
 
-    public WebDriver setChromeCapability() throws MalformedURLException, URISyntaxException {
+    public WebDriver setChromeCapability() throws MalformedURLException {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setCapability("browserVersion", "126.0");
         chromeOptions.setCapability("platformName", "linux");
 
         URL url = new URL(GRID_URL);
-        System.out.println("chromedriver done");
+        System.out.println("chromeDriver done");
         return new RemoteWebDriver(url, chromeOptions);
     }
 
-    public WebDriver setFirefoxCapability() throws MalformedURLException, URISyntaxException {
+    public WebDriver setFirefoxCapability() throws MalformedURLException {
         FirefoxOptions firefoxOptions = new FirefoxOptions();
-        firefoxOptions.setCapability("browserVersion", "latest");
+        firefoxOptions.setCapability("browserVersion", "127.0");
         firefoxOptions.setCapability("platformName", "linux");
-        URL url = new URI(GRID_URL).toURL();
-        System.out.println("ffdriver done");
+
+        URL url = new URL(GRID_URL);
+        System.out.println("ffDriver done");
         return new RemoteWebDriver(url, firefoxOptions);
     }
 
-    public WebDriver setEdgeCapability() throws MalformedURLException, URISyntaxException {
+    public WebDriver setEdgeCapability() throws MalformedURLException {
         EdgeOptions edgeOptions = new EdgeOptions();
-        edgeOptions.setCapability("browserVersion", "latest");
+        edgeOptions.setCapability("browserVersion", "126.0");
         edgeOptions.setCapability("platformName", "linux");
-        URL url = new URI(GRID_URL).toURL();
-        System.out.println("edgedriver done");
+
+        URL url = new URL(GRID_URL);
+        System.out.println("edgeDriver done");
         return new RemoteWebDriver(url, edgeOptions);
     }
 }
