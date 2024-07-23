be4d8817069ff25e627dba223511476f187ef184
Intcognito <130778504+Intcognito@users.noreply.github.com>
2024-07-23 13:43:10 +0200

    implemented static method that returns stream of drivers

--- src/main/java/utils/Util.java
+++ src/main/java/utils/Util.java
@@ -8,11 +8,19 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.stream.Stream;
 
 public class Util {
     private static final String GRID_URL = "http://localhost:4444/";
+    public static Stream<WebDriver> driverProvider() throws MalformedURLException {
+        return Stream.of(
+                setChromeCapability(),
+                setFirefoxCapability(),
+                setEdgeCapability()
+        );
+    }
 
-    public WebDriver setChromeCapability() throws MalformedURLException {
+    public static WebDriver setChromeCapability() throws MalformedURLException {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setCapability("browserVersion", "126.0");
         chromeOptions.setCapability("platformName", "linux");
@@ -22,7 +30,7 @@ public class Util {
         return new RemoteWebDriver(url, chromeOptions);
     }
 
-    public WebDriver setFirefoxCapability() throws MalformedURLException {
+    public static WebDriver setFirefoxCapability() throws MalformedURLException {
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.setCapability("browserVersion", "127.0");
         firefoxOptions.setCapability("platformName", "linux");
@@ -32,7 +40,7 @@ public class Util {
         return new RemoteWebDriver(url, firefoxOptions);
     }
 
-    public WebDriver setEdgeCapability() throws MalformedURLException {
+    public static WebDriver setEdgeCapability() throws MalformedURLException {
         EdgeOptions edgeOptions = new EdgeOptions();
         edgeOptions.setCapability("browserVersion", "126.0");
         edgeOptions.setCapability("platformName", "linux");
