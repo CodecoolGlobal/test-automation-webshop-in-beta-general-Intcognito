29f91321f4cf361ebb948187026173e03d5a1d1a
Intcognito <130778504+Intcognito@users.noreply.github.com>
2024-07-24 17:22:37 +0200

    implemented quantity tests for cart with parameterized credentials

---
+++ src/main/java/pages/CartPage.java
@@ -0,0 +1,40 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class CartPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    @FindBy(xpath = "//a[@class = 'shopping_cart_link']")
+    private WebElement cartLink;
+    @FindBy(xpath = "//button[text() = 'Remove']")
+    private WebElement removeButton;
+    @FindBy(xpath = "//div[@class = 'cart_item']")
+    private List<WebElement> cartItems;
+
+
+    public CartPage (WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(Integer.parseInt(System.getenv("WAIT_DURATION"))));
+        PageFactory.initElements(driver, this);
+    }
+
+    public void openCart() {
+        wait.until(ExpectedConditions.elementToBeClickable(cartLink));
+        cartLink.click();
+    }
+
+    public int countNumberOfProductsInCart() {
+        wait.until(ExpectedConditions.visibilityOf(removeButton));
+        return cartItems.size();
+    }
+
+}
---
+++ src/test/java/cartTests/CartTests.java
@@ -0,0 +1,55 @@
+package cartTests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.WebDriver;
+import pages.CartPage;
+import pages.LoginPage;
+import pages.MainPage;
+import utils.Util;
+
+import java.net.MalformedURLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CartTests {
+    WebDriver firefoxDriver = Util.setFirefoxCapability();
+    LoginPage loginPage = new LoginPage(firefoxDriver);
+    MainPage mainPage = new MainPage(firefoxDriver);
+    CartPage cartPage = new CartPage(firefoxDriver);
+
+    public CartTests() throws MalformedURLException {
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/userCredentials.csv", numLinesToSkip = 1)
+    public void addOneProductToCart(String username, String password) {
+        int numberOfProducts = 1;
+        loginPage.login(username, password);
+
+        mainPage.addProductsToCart(numberOfProducts);
+        cartPage.openCart();
+
+        int actualCartItems = cartPage.countNumberOfProductsInCart();
+        assertEquals(numberOfProducts, actualCartItems);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/userCredentials.csv", numLinesToSkip = 1)
+    public void addThreeProductsToCart(String username, String password) {
+        int numberOfProducts = 3;
+        loginPage.login(username, password);
+
+        mainPage.addProductsToCart(numberOfProducts);
+        cartPage.openCart();
+
+        int actualCartItems = cartPage.countNumberOfProductsInCart();
+        assertEquals(numberOfProducts, actualCartItems);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        firefoxDriver.quit();
+    }
+}
